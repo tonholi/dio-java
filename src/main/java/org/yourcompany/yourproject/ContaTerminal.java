2615dad315739de58f481c59d726bf267b10162d
Luis Gustavo <luisgtnunes@hotmail.com>
2024-08-08 23:37:29 -0300

    Programa final

--- src/main/java/org/yourcompany/yourproject/ContaTerminal.java
+++ src/main/java/org/yourcompany/yourproject/ContaTerminal.java
@@ -1,16 +1,31 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
-
 package org.yourcompany.yourproject;
 
-/**
- *
- * @author Luis
- */
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int numero;
+        String agencia, nomeCliente;
+        double saldo;
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.print("Informe o numero da sua conta: ");
+        numero = scanner.nextInt();
+        scanner.nextLine(); // Consumir a nova linha deixada por nextInt()
+
+        System.out.print("Informe o numero da agência: ");
+        agencia = scanner.next();
+        scanner.nextLine(); // Consumir a nova linha deixada por next()
+
+        System.out.print("Informe o nome do cliente: ");
+        nomeCliente = scanner.nextLine(); // Ler o nome completo
+
+        System.out.print("Informe o deposito inicial do cliente: ");
+        saldo = scanner.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ", conta " + numero + " e seu saldo " + saldo + " já está disponível para saque.");
     }
 }
